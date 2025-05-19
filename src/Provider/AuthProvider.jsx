79dea21dd298c1de0871016ed723966a9b612b59
akrmSujan <sujanban223@gmail.com>
2025-05-20 02:20:00 +0600

    Header Complete

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,29 +1,24 @@
 import React, { createContext, useEffect, useState } from 'react';
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState(() => {
+  const savedTheme = localStorage.getItem('theme');
+  const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  return savedTheme || (systemPrefersDark ? 'dark' : 'light');
+});
 
-    useEffect(() => {
-        const savedTheme = localStorage.getItem('theme');
-        const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+useEffect(() => {
+  const html = document.documentElement;
+  html.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+}, [theme]);
 
-        if (savedTheme) {
-            setTheme(savedTheme);
-        } else if (systemPrefersDark) {
-            setTheme('dark');
-        }
-    }, []);
+const toggleTheme = () => {
+  setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+};
 
-    useEffect(() => {
-        const html = document.documentElement;
-        html.setAttribute('data-theme', theme);
-        localStorage.setItem('theme', theme);
-    }, [theme]);
-    const toggleTheme = () => {
-        console.log('Working')
-        setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-    };
     const AllCom = {
+        theme,
         toggleTheme
     }
     return <AuthContext value={AllCom}>{children}</AuthContext>;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,51 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { AuthContext } from '../Provider/AuthProvider';
+import { Menu, Moon, Sun, X } from 'lucide-react';
+import { NavLink } from 'react-router';
+import { FaGoogle } from 'react-icons/fa6';
+import { FcGoogle } from 'react-icons/fc';
 
 const Header = () => {
-    const {toggleTheme}=use(AuthContext)
+    const { toggleTheme, theme } = use(AuthContext)
+    const [open, setOpen] = useState(false)
     return (
-        <div onClick={toggleTheme}>
-            <p>Header</p>
+        <div className='flex poppins-regular justify-between w-11/12 pt-6 text-xl  mx-auto'>
+            <span onClick={() => setOpen(!open)} className='flex lg:hidden gap-4'>
+                {
+                    open ? <X size={30} className='lg:hidden my-auto'></X> : <Menu size={30} className='lg:hidden my-auto' />
+                }
+                <ul className={`lg:hidden z-20 text-white dark:bg-white bg-gray-700 dark:text-black pr-24 pl-14 min-h-screen py-14 rounded-r-2xl duration-400 absolute ${open ? 'left-0 top-14' : '-left-100 top-14'}`}>
+                    <div className='flex mb-5 flex-col gap-2'>
+                        <p className='my-auto md:hidden text-3xl md:text-4xl lg:text-5xl font-bold'>RooMatch</p>
+                    </div>
+                    <li className='border-b-1 text-gray-100 py-1'><NavLink to={'/'}>Home</NavLink></li>
+                    <li className='border-b-1 text-gray-100 py-1'><NavLink to={'/about'}>Browse Roommates</NavLink></li>
+                    <li className='border-b-1 text-gray-100 py-1' ><NavLink to={'/dash'}>DashBoard</NavLink></li>
+                    <li><button className='btn-block cursor-pointer text-red-600 border-b-1 py-1'>Sign Up</button></li>
+                    <li><button className='btn-block cursor-pointer text-red-600 border-b-1 py-1'>Log In</button></li>
+                    <li><button className='btn-block cursor-pointer  flex justify-between border-b-1 py-1'><FcGoogle className='my-auto' /> Google SignIn</button></li>
+                </ul>
+
+            </span>
+            <div className='lg:flex cursor-pointer gap-2'>
+                <p className='my-auto hidden md:block text-3xl md:text-4xl lg:text-5xl font-bold'>RooMatch</p>
+            </div>
+            <ul className='lg:flex text-gray-500 text-lg font-semibold my-auto hidden gap-5'>
+
+                <li className='my-auto'><NavLink to={'/'}>Home</NavLink></li>
+                <li className='my-auto'><NavLink to={'/about'}>About</NavLink></li>
+                <li className='my-auto'><NavLink to={'/dash'}>DashBoard</NavLink></li>
+                <li className='text-red-400 my-auto cursor-pointer'> Sign Up</li>
+                <li className=' border text-red-400 cursor-pointer rounded-2xl py-0.5 px-1'>Log In</li>
+            </ul>
+            <div className='my-auto'>
+                <button className='cursor-pointer' onClick={toggleTheme}>
+                    {
+                        theme == 'dark' ? <Sun /> : <Moon />
+                    }
+                </button>
+            </div>
+
         </div>
     );
 };
