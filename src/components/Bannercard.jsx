c16db58f2e3a407ea17ce526f403f825ab56c47e
akrmSujan <sujanban223@gmail.com>
2025-05-21 13:33:09 +0600

    Updated Banner

--- src/components/Bannercard.jsx
+++ src/components/Bannercard.jsx
@@ -29,8 +29,8 @@ const Bannercard = ({ info }) => {
                 <h1 className='text-5xl pt-18 lg:pt-0 lg:text-8xl poppins-regular md:text-7xl'>{info.country}</h1>
                 <p className='xl:w-1/2 mt-4 w-2/3'>{info.comment}</p>
                 <div className='flex gap-4 flex-wrap mt-4'>
-                    <button className=' btn bg-orange-600 border-none text-white'>Browse RoomMate</button>
-                    <button className='btn bg-orange-600 border-none text-white'>Add RoomMate</button>
+                    <button className=' btn bg-orange-600 border-none text-white'>Browse City</button>
+                    <button className='btn bg-orange-600 border-none text-white'>Choose Your RoomMate</button>
                 </div>
             </div>
         </div>
